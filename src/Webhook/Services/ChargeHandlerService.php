c3a14d785ceddbf6a1b1bece3a9be133a0ceca88
Wallace Ferreira <wallace.sf87@gmail.com>
2020-01-28 16:14:25 -0300

    :recycle: change way how is treated the charge cancel

--- src/Webhook/Services/ChargeHandlerService.php
+++ src/Webhook/Services/ChargeHandlerService.php
@@ -4,10 +4,13 @@ namespace Mundipagg\Core\Webhook\Services;
 
 use Exception;
 use Mundipagg\Core\Kernel\Aggregates\Charge;
+use Mundipagg\Core\Kernel\Aggregates\ChargeFailed;
 use Mundipagg\Core\Kernel\Exceptions\InvalidParamException;
 use Mundipagg\Core\Kernel\Exceptions\NotFoundException;
+use Mundipagg\Core\Kernel\Factories\ChargeFactory;
 use Mundipagg\Core\Kernel\Services\APIService;
 use Mundipagg\Core\Kernel\Services\ChargeService;
+use Mundipagg\Core\Kernel\Services\ChargeFailedService;
 use Mundipagg\Core\Webhook\Aggregates\Webhook;
 
 final class ChargeHandlerService
@@ -49,31 +52,47 @@ final class ChargeHandlerService
     {
         $this->build($webhook->getComponent());
 
-//        $multiMeiosCanceled = $this->tryCancelMultiMeios($webhook);
-//        if ($multiMeiosCanceled) {
-//            return $multiMeiosCanceled;
-//        }
+        $multiMeiosCanceled = $this->tryCancelMultiMethods($webhook);
 
-        return $this->listChargeHandleService->handle($webhook);
+        return array_merge(
+            $multiMeiosCanceled,
+            $this->listChargeHandleService->handle($webhook)
+        );
     }
 
+    /**
+     * @param Webhook $webhook
+     * @return array|\Mundipagg\Core\Kernel\Responses\ServiceResponse|null
+     * @throws InvalidParamException
+     */
     public function tryCancelMultiMethods(Webhook $webhook)
     {
         /** @var Charge $charge  */
         $charge = $webhook->getEntity();
 
-        $chargeService = new ChargeService();
-        $apiService = new APIService();
+        $chargeFailedService = new ChargeFailedService();
 
-        $order = $apiService->getOrder($charge->getOrderId());
+        /** @var ChargeFailed $chargeFailedList */
+        $chargeFailedList = $chargeFailedService->findByOrderId($charge->getOrderId());
 
-        $chargeList = $chargeService->checkHasChargesPaidBetweenFailed(
-            $order->getCharges()
+        $chargeListPaid = $chargeFailedService->checkHasChargesPaidBetweenFailed(
+            $chargeFailedList
         );
 
+        if (empty($chargeListPaid)) {
+            return null;
+        }
+
+        $chargeFactory = new ChargeFactory();
+        $chargeList = [];
+        foreach ($chargeListPaid as $chargePaid) {
+            $chargeList[] = $chargeFactory->createFromChargeFailed($chargePaid);
+        }
+
         $listResponse = [];
+        $chargeService = new ChargeService();
         foreach ($chargeList as $charge) {
-            $listResponse = $chargeService->cancelJustInMundiPagg($charge);
+            $listResponse[] = $chargeService->cancelJustAtMundiPagg($charge);
         }
 
         return $listResponse;
