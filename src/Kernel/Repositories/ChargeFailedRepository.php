7d8149cab58e0ca54658a4dcf3de92ca06d71d0d
Wallace Ferreira <wallace.sf87@gmail.com>
2020-01-28 16:08:37 -0300

    :zap: implementing method to find chargefailed by orderid

--- src/Kernel/Repositories/ChargeFailedRepository.php
+++ src/Kernel/Repositories/ChargeFailedRepository.php
@@ -5,73 +5,21 @@ namespace Mundipagg\Core\Kernel\Repositories;
 use Mundipagg\Core\Kernel\Abstractions\AbstractDatabaseDecorator;
 use Mundipagg\Core\Kernel\Abstractions\AbstractEntity;
 use Mundipagg\Core\Kernel\Abstractions\AbstractRepository;
-use Mundipagg\Core\Kernel\Aggregates\Charge;
-use Mundipagg\Core\Kernel\Factories\ChargeFactory;
+use Mundipagg\Core\Kernel\Factories\ChargeFailedFactory;
 use Mundipagg\Core\Kernel\ValueObjects\AbstractValidString;
 use Mundipagg\Core\Kernel\ValueObjects\Id\OrderId;
 
 final class ChargeFailedRepository extends AbstractRepository
 {
-
-    public function findByOrderId(OrderId $orderId)
-    {
-        $chargeTable = $this->db->getTable(AbstractDatabaseDecorator::TABLE_CHARGE);
-        $transactionTable = $this->db->getTable(AbstractDatabaseDecorator::TABLE_TRANSACTION);
-
-        $id = $orderId->getValue();
-
-        $query = "
-            SELECT 
-                c.*, 
-                GROUP_CONCAT(t.id) as tran_id, 
-                GROUP_CONCAT(t.mundipagg_id) as tran_mundipagg_id,
-                GROUP_CONCAT(t.charge_id) as tran_charge_id,
-                GROUP_CONCAT(t.amount) as tran_amount,
-                GROUP_CONCAT(t.paid_amount) as tran_paid_amount,
-                GROUP_CONCAT(t.acquirer_name) as tran_acquirer_name,                
-                GROUP_CONCAT(t.acquirer_message) as tran_acquirer_message,                
-                GROUP_CONCAT(t.acquirer_nsu) as tran_acquirer_nsu,                
-                GROUP_CONCAT(t.acquirer_tid) as tran_acquirer_tid,                
-                GROUP_CONCAT(t.acquirer_auth_code) as tran_acquirer_auth_code,                
-                GROUP_CONCAT(t.type) as tran_type,
-                GROUP_CONCAT(t.status) as tran_status,
-                GROUP_CONCAT(t.created_at) as tran_created_at,
-                GROUP_CONCAT(t.boleto_url) as tran_boleto_url,
-                GROUP_CONCAT(t.card_data SEPARATOR '---') as tran_card_data
-            FROM
-                $chargeTable as c 
-                LEFT JOIN $transactionTable as t  
-                  ON c.mundipagg_id = t.charge_id 
-            WHERE c.order_id = '$id'
-            GROUP BY c.id;
-        ";
-
-        $result = $this->db->fetch($query);
-
-        if ($result->num_rows === 0) {
-            return [];
-        }
-
-        $factory = new ChargeFactory();
-
-        $charges = [];
-        foreach ($result->rows as $row) {
-            $charges[] = $factory->createFromDbData($row);
-        }
-
-        return $charges;
-    }
-
     /**
-     *
      * @param  \Mundipagg\Core\Kernel\Aggregates\ChargeFailed $object
      * @throws \Exception
      */
     protected function create(AbstractEntity &$object)
     {
-        $chargeFailedTable = $this->db->getTable(AbstractDatabaseDecorator::TABLE_CHARGE_FAILED);
-
-        $simpleObject = json_decode(json_encode($object));
+        $chargeFailedTable = $this->db->getTable(
+            AbstractDatabaseDecorator::TABLE_CHARGE_FAILED
+        );
 
         $query = "
           INSERT INTO 
@@ -88,11 +36,11 @@ final class ChargeFailedRepository extends AbstractRepository
 
         $query .= "
             (
-                '{$simpleObject->mundipaggId}',
-                '{$simpleObject->orderId}',
-                '{$simpleObject->code}',
-                {$simpleObject->amount},
-                '{$simpleObject->status}'           
+                '{$object->getMundipaggId()->getValue()}',
+                '{$object->getOrderId()->getValue()}',
+                '{$object->getCode()}',
+                {$object->getAmount()},
+                '{$object->getStatus()->getStatus()}'           
             );
         ";
 
@@ -101,31 +49,7 @@ final class ChargeFailedRepository extends AbstractRepository
 
     protected function update(AbstractEntity &$object)
     {
-        $charge = json_decode(json_encode($object));
-        $chargeFailedTable = $this->db->getTable(AbstractDatabaseDecorator::TABLE_CHARGE);
-
-        $metadata = json_encode($charge->metadata);
-
-        $query = "
-            UPDATE $chargeTable SET
-              amount = {$charge->amount},
-              paid_amount = {$charge->paidAmount},                         
-              refunded_amount = {$charge->refundedAmount},                         
-              canceled_amount = {$charge->canceledAmount},
-              status = '{$charge->status}',
-              metadata = '{$metadata}',
-              customer_id = '{$charge->customerId}'
-            WHERE id = {$charge->id}
-        ";
-
-        $this->db->query($query);
-
-        //update Transactions;
-        $transactionRepository = new TransactionRepository();
-        foreach ($object->getTransactions() as $transaction) {
-            $transactionRepository->save($transaction);
-            $object->updateTransaction($transaction, true);
-        }
+        // TODO: Implement update() method.
     }
 
     public function delete(AbstractEntity $object)
@@ -145,36 +69,22 @@ final class ChargeFailedRepository extends AbstractRepository
 
     public function findByMundipaggId(AbstractValidString $mundipaggId)
     {
-        $chargeTable = $this->db->getTable(AbstractDatabaseDecorator::TABLE_CHARGE);
-        $transactionTable = $this->db->getTable(AbstractDatabaseDecorator::TABLE_TRANSACTION);
 
-        $id = $mundipaggId->getValue();
+    }
 
-        $query = "
-            SELECT 
-                c.*, 
-                GROUP_CONCAT(t.id) as tran_id, 
-                GROUP_CONCAT(t.mundipagg_id) as tran_mundipagg_id,
-                GROUP_CONCAT(t.charge_id) as tran_charge_id,
-                GROUP_CONCAT(t.amount) as tran_amount,
-                GROUP_CONCAT(t.paid_amount) as tran_paid_amount,
-                GROUP_CONCAT(t.acquirer_name) as tran_acquirer_name,                
-                GROUP_CONCAT(t.acquirer_message) as tran_acquirer_message,                
-                GROUP_CONCAT(t.acquirer_nsu) as tran_acquirer_nsu,                
-                GROUP_CONCAT(t.acquirer_tid) as tran_acquirer_tid,                
-                GROUP_CONCAT(t.acquirer_auth_code) as tran_acquirer_auth_code,                
-                GROUP_CONCAT(t.type) as tran_type,
-                GROUP_CONCAT(t.status) as tran_status,
-                GROUP_CONCAT(t.created_at) as tran_created_at,
-                GROUP_CONCAT(t.boleto_url) as tran_boleto_url,
-                GROUP_CONCAT(t.card_data SEPARATOR '---') as tran_card_data
-            FROM
-                $chargeTable as c 
-                LEFT JOIN $transactionTable as t  
-                  ON c.mundipagg_id = t.charge_id 
-            WHERE c.mundipagg_id = '$id'
-            GROUP BY c.id;
-        ";
+    /**
+     * @param OrderId $orderId
+     * @return AbstractEntity|\Mundipagg\Core\Kernel\Aggregates\ChargeFailed|null
+     * @throws \Mundipagg\Core\Kernel\Exceptions\InvalidParamException
+     */
+    public function findByOrderId(OrderId $orderId)
+    {
+        $chargeFailedTable = $this->db->getTable(
+            AbstractDatabaseDecorator::TABLE_CHARGE_FAILED
+        );
+
+        $query = "SELECT * FROM `{$chargeFailedTable}` ";
+        $query .= "WHERE order_id = '{$orderId->getValue()}';";
 
         $result = $this->db->fetch($query);
 
@@ -182,8 +92,11 @@ final class ChargeFailedRepository extends AbstractRepository
             return null;
         }
 
-        $factory = new ChargeFactory();
-
-        return $factory->createFromDbData($result->row);
+        $factory = new ChargeFailedFactory();
+        $chargeListFailed = [];
+        foreach ($result->rows as $chargeFailedDb) {
+            $chargeListFailed[] = $factory->createFromDbData($chargeFailedDb);
+        }
+        return $chargeListFailed;
     }
 }
