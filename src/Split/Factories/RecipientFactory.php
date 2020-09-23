42af10bd3ae109d0bcafe3fd33947a5da651811b
Wallace Ferreira <wallace.sf87@gmail.com>
2020-09-23 13:15:16 -0300

    🎨 adds transfer settings

--- src/Split/Factories/RecipientFactory.php
+++ src/Split/Factories/RecipientFactory.php
@@ -2,25 +2,24 @@
 
 namespace Mundipagg\Core\Split\Factories;
 
+use Exception;
 use Mundipagg\Core\Kernel\Abstractions\AbstractEntity;
+use Mundipagg\Core\Kernel\Exceptions\InvalidParamException;
 use Mundipagg\Core\Kernel\Helper\Hydrator;
-use Mundipagg\Core\Kernel\Interfaces\FactoryInterface;
+use Mundipagg\Core\Kernel\Interfaces\FactoryCreateFromDbDataInterface;
 use Mundipagg\Core\Split\Aggregates\Recipient;
 use Mundipagg\Core\Split\ValueObjects\Id\RecipientId;
 use Mundipagg\Core\Split\ValueObjects\StatusRecipient;
 use ReflectionException;
 
-class RecipientFactory implements FactoryInterface
+class RecipientFactory implements FactoryCreateFromDbDataInterface
 {
-    public function createFromPostData($postData)
-    {
-        // TODO: Implement createFromPostData() method.
-    }
-
     /**
      * @param array $dbData
      * @return AbstractEntity|Recipient
      * @throws ReflectionException
+     * @throws InvalidParamException
+     * @throws Exception
      */
     public function createFromDbData($dbData)
     {
@@ -33,6 +32,7 @@ class RecipientFactory implements FactoryInterface
         $recipient->setMundipaggId(new RecipientId($dbData['mundipagg_id']));
         $recipient->setBankAccount($dbData['bank_account']);
         $recipient->setDocument($dbData['document']);
+        $recipient->setTransferSettings($dbData['transfer_settings']);
 
         return $recipient;
     }
