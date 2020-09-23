067a39e26fc71a0e0554c1c10d869a78fd82647c
Wallace Ferreira <wallace.sf87@gmail.com>
2020-09-23 13:13:12 -0300

    improve factory bank account

--- src/Split/Factories/BankAccountFactory.php
+++ src/Split/Factories/BankAccountFactory.php
@@ -4,18 +4,14 @@ namespace Mundipagg\Core\Split\Factories;
 
 use Mundipagg\Core\Kernel\Abstractions\AbstractEntity;
 use Mundipagg\Core\Kernel\Helper\Hydrator;
-use Mundipagg\Core\Kernel\Interfaces\FactoryInterface;
+use Mundipagg\Core\Kernel\Interfaces\FactoryCreateFromDbDataInterface;
+use Mundipagg\Core\Kernel\ValueObjects\Type;
 use Mundipagg\Core\Split\Aggregates\BankAccount;
-use Mundipagg\Core\Split\ValueObjects\Id\RecipientId;
+use Mundipagg\Core\Split\ValueObjects\TypeBankAccount;
 use ReflectionException;
 
-class BankAccountFactory implements FactoryInterface
+class BankAccountFactory implements FactoryCreateFromDbDataInterface
 {
-    public function createFromPostData($postData)
-    {
-        // TODO: Implement createFromPostData() method.
-    }
-
     /**
      * @param array $dbData
      * @return AbstractEntity|BankAccount
@@ -26,6 +22,10 @@ class BankAccountFactory implements FactoryInterface
         /**
          * @var BankAccount $bankAccount
          */
-        return Hydrator::hidrator($dbData, new BankAccount());
+        $bankAccount = Hydrator::hidrator($dbData, new BankAccount());
+        $bankAccount->setType(new TypeBankAccount($dbData['type']));
+        $bankAccount->setHolderType(new Type($dbData['holder_type']));
+
+        return $bankAccount;
     }
 }
