94e0c137b274fedf42fe9065c5828359fb225724
Wallace Ferreira <wallace.sf87@gmail.com>
2020-09-23 13:11:57 -0300

    🎨 become the parent construct explict

--- src/Split/ValueObjects/Id/RecipientId.php
+++ src/Split/ValueObjects/Id/RecipientId.php
@@ -2,10 +2,21 @@
 
 namespace Mundipagg\Core\Split\ValueObjects\Id;
 
+use Mundipagg\Core\Kernel\Exceptions\InvalidParamException;
 use Mundipagg\Core\Kernel\ValueObjects\AbstractValidString;
 
 class RecipientId extends AbstractValidString
 {
+    /**
+     * RecipientId constructor.
+     * @param string $mundipaggId
+     * @throws InvalidParamException
+     */
+    public function __construct($mundipaggId)
+    {
+        parent::__construct($mundipaggId);
+    }
+
     protected function validateValue($value)
     {
         return preg_match('/^rp_\w{16}$/', $value) === 1;
